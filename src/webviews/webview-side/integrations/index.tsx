acea24bd144e40158ae99ea7b4ff9e0b150bbc4a
jankuca <jan@jankuca.com>
2025-10-16 13:19:49 +0200

    polish integration html base

--- src/webviews/webview-side/integrations/index.tsx
+++ src/webviews/webview-side/integrations/index.tsx
@@ -1,6 +1,3 @@
-// eslint-disable-next-line import/order
-import '../common/index.css';
-
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
@@ -8,15 +5,15 @@ import { IVsCodeApi } from '../react-common/postOffice';
 import { detectBaseTheme } from '../react-common/themeDetector';
 import { IntegrationPanel } from './IntegrationPanel';
 
+import '../common/index.css';
+
 // This special function talks to vscode from a web panel
-export declare function acquireVsCodeApi(): IVsCodeApi;
+declare function acquireVsCodeApi(): IVsCodeApi;
 
 const baseTheme = detectBaseTheme();
 const vscodeApi = acquireVsCodeApi();
 
-/* eslint-disable  */
 ReactDOM.render(
     <IntegrationPanel baseTheme={baseTheme} vscodeApi={vscodeApi} />,
     document.getElementById('root') as HTMLElement
 );
-

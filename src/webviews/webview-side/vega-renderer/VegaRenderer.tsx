64761dd6de01aac7e51dd8a45c3a121193f7bbc9
OlegWock <olegwock@gmail.com>
2025-10-16 10:30:12 +0200

    Fix formatting

--- src/webviews/webview-side/vega-renderer/VegaRenderer.tsx
+++ src/webviews/webview-side/vega-renderer/VegaRenderer.tsx
@@ -1,50 +1,48 @@
-import { chartColors10, chartColors20, deepnoteBlues } from './colors'
-import React, { memo, useLayoutEffect } from 'react'
-import { Vega } from 'react-vega'
-import { vega } from 'vega-embed'
+import { chartColors10, chartColors20, deepnoteBlues } from './colors';
+import React, { memo, useLayoutEffect } from 'react';
+import { Vega } from 'react-vega';
+import { vega } from 'vega-embed';
 
-import { numberFormats } from './number-formats'
-
+import { numberFormats } from './number-formats';
 
 export interface VegaRendererProps {
-  spec: Record<string, unknown>
-  renderer?: 'svg' | 'canvas'
+    spec: Record<string, unknown>;
+    renderer?: 'svg' | 'canvas';
 }
 
 export const VegaRenderer = memo(function VegaRenderer(props: VegaRendererProps) {
-  const { renderer, spec } = props
+    const { renderer, spec } = props;
 
+    useLayoutEffect(function registerCustomVegaConfigsOnce() {
+        if (vega.expressionFunction('numberFormatFromNumberType')) {
+            return;
+        }
 
-  useLayoutEffect(function registerCustomVegaConfigsOnce() {
-    if (vega.expressionFunction('numberFormatFromNumberType')) {
-      return
-    }
+        vega.expressionFunction(
+            'numberFormatFromNumberType',
+            (value: number, params: { type: string; decimals: number | null }) => {
+                // NOTE: default params.type to "default" value to ensure that eg "Sum of" labels are displayed in custom tooltip
+                return numberFormats[params?.type ?? 'default']?.formatter(value, params?.decimals);
+            }
+        );
 
-    vega.expressionFunction(
-      'numberFormatFromNumberType',
-      (value: number, params: { type: string; decimals: number | null }) => {
-        // NOTE: default params.type to "default" value to ensure that eg "Sum of" labels are displayed in custom tooltip
-        return numberFormats[params?.type ?? 'default']?.formatter(value, params?.decimals)
-      }
-    )
+        vega.scheme('deepnote10', chartColors10);
+        vega.scheme('deepnote20', chartColors20);
+        vega.scheme('deepnote_blues', deepnoteBlues);
+    }, []);
 
-    vega.scheme('deepnote10', chartColors10)
-    vega.scheme('deepnote20', chartColors20)
-    vega.scheme('deepnote_blues', deepnoteBlues)
-  }, [])
+    //   const unfreezedSpec = useMemo(() => clone(addRuntimeDataToVegaSpec(spec, theme)), [spec, theme])
+    const unfreezedSpec = spec;
 
-//   const unfreezedSpec = useMemo(() => clone(addRuntimeDataToVegaSpec(spec, theme)), [spec, theme])
-  const unfreezedSpec = spec
-
-  return (
-    <Vega
-      spec={unfreezedSpec}
-      renderer={renderer}
-      actions={false}
-      style={{
-        height: '100%',
-        width: '100%',
-      }}
-    />
-  )
-})
+    return (
+        <Vega
+            spec={unfreezedSpec}
+            renderer={renderer}
+            actions={false}
+            style={{
+                height: '100%',
+                width: '100%'
+            }}
+        />
+    );
+});

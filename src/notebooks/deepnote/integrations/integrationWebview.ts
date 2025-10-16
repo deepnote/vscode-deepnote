c02a7a08c2415f7bc3048a1f810eaae1d0c972b6
jankuca <jan@jankuca.com>
2025-10-16 13:33:56 +0200

    remove unsafe flag from integration webview csp

--- src/notebooks/deepnote/integrations/integrationWebview.ts
+++ src/notebooks/deepnote/integrations/integrationWebview.ts
@@ -252,7 +252,7 @@ export class IntegrationWebviewProvider implements IIntegrationWebviewProvider {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource} 'unsafe-inline'; script-src 'nonce-${nonce}'; font-src ${webview.cspSource};">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}'; font-src ${webview.cspSource};">
     <link rel="stylesheet" href="${codiconUri}">
     <link rel="stylesheet" href="${styleUri}">
     <title>Deepnote Integrations</title>

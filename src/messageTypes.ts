93f664f2e5dc870e185613de4992cefe235cb76b
jankuca <jan@jankuca.com>
2025-10-29 19:50:33 +0100

    make it clear comparison value is a variable

--- src/messageTypes.ts
+++ src/messageTypes.ts
@@ -472,6 +472,7 @@ export type LocalizedMessages = {
     comparisonTitleLabel: string;
     comparisonTitlePlaceholder: string;
     comparisonTitleHelp: string;
+    comparisonValueHelp: string;
     comparisonFormatLabel: string;
     comparisonFormatHelp: string;
 };

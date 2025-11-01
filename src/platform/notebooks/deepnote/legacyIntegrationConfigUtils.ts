ed2a8859a858e143f9a66b9ba5ca8a0c524dd061
jankuca <jan@jankuca.com>
2025-11-01 16:06:44 +0100

    add legacy integration config upgrading logic

---
+++ src/platform/notebooks/deepnote/legacyIntegrationConfigUtils.ts
@@ -0,0 +1,95 @@
+import { LegacyIntegrationConfig, LegacyIntegrationType } from './integrationTypes';
+import {
+    BigQueryAuthMethods,
+    DatabaseIntegrationConfig,
+    databaseMetadataSchemasByType,
+    SnowflakeAuthMethods
+} from '@deepnote/database-integrations';
+import { SnowflakeAuthMethods as LegacySnowflakeAuthMethods } from './snowflakeAuthConstants';
+
+// NOTE: We need a way to upgrade existing configurations to the new format of deepnote/database-integrations.
+type VersionedDatabaseIntegrationConfig = DatabaseIntegrationConfig & { version: 1 };
+
+export async function upgradeLegacyIntegrationConfig(
+    config: LegacyIntegrationConfig
+): Promise<VersionedDatabaseIntegrationConfig | null> {
+    switch (config.type) {
+        case LegacyIntegrationType.Postgres: {
+            const metadata = databaseMetadataSchemasByType.pgsql.safeParse({
+                host: config.host,
+                port: config.port ? String(config.port) : undefined,
+                database: config.database,
+                user: config.username,
+                password: config.password,
+                sslEnabled: config.ssl
+            }).data;
+
+            return metadata
+                ? {
+                      version: 1,
+                      id: config.id,
+                      name: config.name,
+                      type: 'pgsql',
+                      metadata
+                  }
+                : null;
+        }
+        case LegacyIntegrationType.BigQuery: {
+            const metadata = databaseMetadataSchemasByType['big-query'].safeParse({
+                authMethod: BigQueryAuthMethods.ServiceAccount,
+                service_account: config.credentials
+            }).data;
+
+            return metadata
+                ? {
+                      version: 1,
+                      id: config.id,
+                      name: config.name,
+                      type: 'big-query',
+                      metadata
+                  }
+                : null;
+        }
+        case LegacyIntegrationType.Snowflake: {
+            const metadata = (() => {
+                switch (config.authMethod) {
+                    case LegacySnowflakeAuthMethods.PASSWORD:
+                        return databaseMetadataSchemasByType.snowflake.safeParse({
+                            authMethod: SnowflakeAuthMethods.Password,
+                            accountName: config.account,
+                            warehouse: config.warehouse,
+                            database: config.database,
+                            role: config.role,
+                            username: config.username,
+                            password: config.password
+                        }).data;
+                    case LegacySnowflakeAuthMethods.SERVICE_ACCOUNT_KEY_PAIR:
+                        return databaseMetadataSchemasByType.snowflake.safeParse({
+                            authMethod: SnowflakeAuthMethods.ServiceAccountKeyPair,
+                            accountName: config.account,
+                            warehouse: config.warehouse,
+                            database: config.database,
+                            role: config.role,
+                            username: config.username,
+                            privateKey: config.privateKey,
+                            privateKeyPassphrase: config.privateKeyPassphrase
+                        }).data;
+                    default:
+                        return null;
+                }
+            })();
+
+            return metadata
+                ? {
+                      version: 1,
+                      id: config.id,
+                      name: config.name,
+                      type: 'snowflake',
+                      metadata
+                  }
+                : null;
+        }
+        default:
+            return null;
+    }
+}

1deacdc89cbb5019dc1d44234240fe2aa1688d24
jankuca <jan@jankuca.com>
2025-11-03 14:28:56 +0100

    remove https from img-src csp in integration webview

--- src/notebooks/deepnote/integrations/integrationWebview.ts
+++ src/notebooks/deepnote/integrations/integrationWebview.ts
@@ -660,7 +660,7 @@ export class IntegrationWebviewProvider implements IIntegrationWebviewProvider {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource} https: data:; style-src ${webview.cspSource} 'unsafe-inline'; script-src 'nonce-${nonce}'; font-src ${webview.cspSource};">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource} data:; style-src ${webview.cspSource} 'unsafe-inline'; script-src 'nonce-${nonce}'; font-src ${webview.cspSource};">
     <link rel="stylesheet" href="${codiconUri}">
     <title>Deepnote Integrations</title>
 </head>

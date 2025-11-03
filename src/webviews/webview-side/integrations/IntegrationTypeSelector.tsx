3bbc0bf52e8b368c849d11cdd944b86a0c2e707d
jankuca <jan@jankuca.com>
2025-11-03 13:45:29 +0100

    add svg logos of integrations

--- src/webviews/webview-side/integrations/IntegrationTypeSelector.tsx
+++ src/webviews/webview-side/integrations/IntegrationTypeSelector.tsx
@@ -2,6 +2,27 @@ import * as React from 'react';
 import { getLocString } from '../react-common/locReactSide';
 import { ConfigurableDatabaseIntegrationType } from './types';
 
+// Import integration logos
+/* eslint-disable @typescript-eslint/no-require-imports */
+const postgresqlLogo: string = require('./icons/postgresql.svg');
+const mysqlLogo: string = require('./icons/mysql.svg');
+const mariadbLogo: string = require('./icons/mariadb.svg');
+const mongodbLogo: string = require('./icons/mongodb.svg');
+const sqlServerLogo: string = require('./icons/sql-server.svg');
+const bigqueryLogo: string = require('./icons/bigquery.svg');
+const snowflakeLogo: string = require('./icons/snowflake.svg');
+const alloydbLogo: string = require('./icons/alloydb.svg');
+const spannerLogo: string = require('./icons/spanner.svg');
+const materializeLogo: string = require('./icons/materialize.svg');
+const clickhouseLogo: string = require('./icons/clickhouse.svg');
+const athenaLogo: string = require('./icons/athena.svg');
+const redshiftLogo: string = require('./icons/redshift.svg');
+const databricksLogo: string = require('./icons/databricks.svg');
+const dremioLogo: string = require('./icons/dremio.svg');
+const mindsdbLogo: string = require('./icons/mindsdb.svg');
+const trinoLogo: string = require('./icons/trino.svg');
+/* eslint-enable @typescript-eslint/no-require-imports */
+
 export interface IIntegrationTypeSelectorProps {
     onSelectType: (type: ConfigurableDatabaseIntegrationType) => void;
 }
@@ -16,87 +37,87 @@ const INTEGRATION_TYPES: IntegrationTypeInfo[] = [
     {
         type: 'pgsql',
         label: 'PostgreSQL',
-        icon: '🐘'
+        icon: postgresqlLogo
     },
     {
         type: 'mysql',
         label: 'MySQL',
-        icon: '🐬'
+        icon: mysqlLogo
     },
     {
         type: 'mariadb',
         label: 'MariaDB',
-        icon: '🦭'
+        icon: mariadbLogo
     },
     {
         type: 'mongodb',
         label: 'MongoDB',
-        icon: '🍃'
+        icon: mongodbLogo
     },
     {
         type: 'sql-server',
         label: 'Microsoft SQL Server',
-        icon: '🗄️'
+        icon: sqlServerLogo
     },
     {
         type: 'big-query',
         label: 'Google BigQuery',
-        icon: '📊'
+        icon: bigqueryLogo
     },
     {
         type: 'snowflake',
         label: 'Snowflake',
-        icon: '❄️'
+        icon: snowflakeLogo
     },
     {
         type: 'alloydb',
         label: 'Google AlloyDB',
-        icon: '🔷'
+        icon: alloydbLogo
     },
     {
         type: 'spanner',
         label: 'Google Cloud Spanner',
-        icon: '🔧'
+        icon: spannerLogo
     },
     {
         type: 'materialize',
         label: 'Materialize',
-        icon: '⚡'
+        icon: materializeLogo
     },
     {
         type: 'clickhouse',
         label: 'ClickHouse',
-        icon: '🏠'
+        icon: clickhouseLogo
     },
     {
         type: 'athena',
         label: 'Amazon Athena',
-        icon: '🏛️'
+        icon: athenaLogo
     },
     {
         type: 'redshift',
         label: 'Amazon Redshift',
-        icon: '🔴'
+        icon: redshiftLogo
     },
     {
         type: 'databricks',
         label: 'Databricks',
-        icon: '🧱'
+        icon: databricksLogo
     },
     {
         type: 'dremio',
         label: 'Dremio',
-        icon: '🚀'
+        icon: dremioLogo
     },
     {
         type: 'mindsdb',
         label: 'MindsDB',
-        icon: '🧠'
+        icon: mindsdbLogo
     },
     {
         type: 'trino',
         label: 'Trino',
-        icon: '⚙️'
+        icon: trinoLogo
     }
 ];
 
@@ -112,7 +133,9 @@ export const IntegrationTypeSelector: React.FC<IIntegrationTypeSelectorProps> = 
                         className="integration-type-card"
                         onClick={() => onSelectType(integrationInfo.type)}
                     >
-                        <div className="integration-type-icon">{integrationInfo.icon}</div>
+                        <div className="integration-type-icon">
+                            <img src={integrationInfo.icon} alt={integrationInfo.label} />
+                        </div>
                         <div className="integration-type-label">{integrationInfo.label}</div>
                         <div className="integration-type-category">
                             {getLocString('integrationsDatabase', 'Database')}
@@ -123,4 +146,3 @@ export const IntegrationTypeSelector: React.FC<IIntegrationTypeSelectorProps> = 
         </div>
     );
 };
-
---
+++ src/webviews/webview-side/integrations/images.d.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/* eslint-disable */
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+
